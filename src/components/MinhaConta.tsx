c9a3f6eac36ffd2fb01a137d3ee739755e42c32f
Rein4ldoAlv3s <naldos15@hotmail.com>
2024-12-10 14:49:33 -0300

    form teste

--- src/components/MinhaConta.tsx
+++ src/components/MinhaConta.tsx
@@ -30,13 +30,15 @@ const MinhaConta = () => {
                             className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                         />
                     </div>
-                    <div className='mt-2'>
+                    <div className='mt-3'>
                         <button className='bg-black rounded-md text-slate-300 px-3 py-1.5 w-full'>Entrar</button>
                     </div>
 
                     <hr className='mt-3 w-1/2 m-auto' />
-                    <h2>Novo na Gyn Gamer?</h2>
-                    <a href="">Cadastre-se!</a>
+                    <div className='flex justify-center mt-2'>
+                        <h2 className='text-customWhite mr-2'>Novo na Gyn Gamer?</h2>
+                        <a className='text-customWhite underline' href="">Cadastre-se!</a>
+                    </div>
                 </div>
             </div>
         </div>
