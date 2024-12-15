c7dd393a2e5f5d8c16c5925d5d189153aa29372e
Rein4ldoAlv3s <naldos15@hotmail.com>
2024-12-15 20:36:53 -0300

    form fix

--- src/components/MinhaConta.tsx
+++ src/components/MinhaConta.tsx
@@ -19,7 +19,8 @@ const MinhaConta = () => {
         password: password
     }
 
-    function authentic() {
+    function authentic(e: any) {
+        e.preventDefault();
         axios.post('http://localhost:3000/login', user)
             .then(function (response) {
                 console.log(response);
@@ -40,46 +41,40 @@ const MinhaConta = () => {
     // }, [email]);
 
     return (
-        <div className="mt-10 grid grid-cols-1 max-w-lg mx-auto ">
+        <form onSubmit={(e) => authentic(e)} className="mt-10 grid grid-cols-1 max-w-lg mx-auto ">
             <h1 className='text-white text-3xl text-center'>Acesse sua conta</h1>
             <label htmlFor="email" className="block text-sm/6 font-medium text-white">
                 E-mail
             </label>
-            <div className="mt-2">
-                <input
-                    id="email"
-                    required
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                    onChange={(e: any) => setEmail(e.target.value)}
-                />
-            </div>
+            <input
+                id="email"
+                required
+                name="email"
+                type="email"
+                autoComplete="email"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 mt-2"
+                onChange={(e: any) => setEmail(e.target.value)}
+            />
             <label htmlFor="password" className="mt-2 block text-sm/6 font-medium text-white">
                 Senha
             </label>
-            <div className="mt-2">
-                <input
-                    id="password"
-                    required
-                    name="password"
-                    type="password"
-                    autoComplete="password"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                    onChange={(e: any) => setPassword(e.target.value)}
-                />
-            </div>
-            <div className='mt-3'>
-                <button type='submit' onClick={() => authentic()} className='bg-black rounded-md text-slate-300 px-3 py-1.5 w-full'>Entrar</button>
-            </div>
+            <input
+                id="password"
+                required
+                name="password"
+                type="password"
+                autoComplete="password"
+                className="mt-2 block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                onChange={(e: any) => setPassword(e.target.value)}
+            />
+            <button type='submit' className='mt-3 bg-black rounded-md text-slate-300 px-3 py-1.5 w-full'>Entrar</button>
 
             <hr className='mt-3 w-1/2 m-auto' />
             <div className='flex justify-center mt-2'>
                 <h2 className='text-customWhite mr-2'>Novo na Gyn Gamer?</h2>
                 <a className='text-customWhite underline' href="/cadastro">Cadastre-se</a>
             </div>
-        </div>
+        </form>
     )
 }
 
