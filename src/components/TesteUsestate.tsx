fc134a4f19ac79db3a84c4367f8cb64f1ee93323
Rein4ldoAlv3s <naldos15@hotmail.com>
2024-12-19 21:34:01 -0300

    Alterar nome variavel

--- src/components/TesteUsestate.tsx
+++ src/components/TesteUsestate.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TesteUsestate = () => {
+
+
+
     return (
-        <div>TesteUsestate</div>
+        <div>
+            <h1 className='text-white'>componente 1</h1>
+            <Comp2 />
+        </div>
     )
 }
+
+const Comp2 = () => {
+
+
+
+    const setValue = () => {
+        sessionStorage.setItem("nome", "Reinaldo")
+    }
+
+    return (
+        <div>
+            <h1 className='text-white'>componente 2</h1>
+            <button className='mt-5 text-white bg-black' onClick={setValue}>Alterar nome variavel</button>
+        </div>
+    )
+
+}
+
 
 export default TesteUsestate
