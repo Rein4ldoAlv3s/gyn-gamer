443e0fac75c55de5ec51ac954bc9b7ad85a3ed87
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-01-01 15:00:32 -0300

    botoes com rotas autorizadas

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -72,47 +72,58 @@ const Navbar = () => {
             </MenuButton>
             <MenuItems transition modal={false} className="absolute text-white right-0 mt-2 w-56 bg-white rounded-md shadow-lg ring-1  ring-black ring-opacity-5 z-10 transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0">
               <div className="py-1">
-                <MenuItem>
-                  {({ active }) => (
-                    <Link
-                      to="/login"
-                      className={`block px-4 py-2 text-sm ${active ? 'bg-black' : 'text-gray-700'}`}
-                    >
-                      Acesse sua Conta
-                    </Link>
-                  )}
-                </MenuItem>
-                <MenuItem>
-                  {({ active }) => (
-                    <Link
-                      to="/cadastro"
-                      className={`block px-4 py-2 text-sm ${active ? 'bg-black' : 'text-gray-700'}`}
-                    >
-                      Criar Conta
-                    </Link>
-                  )}
-                </MenuItem>
-                <MenuItem>
-                  {({ active }) => (
-                    <Link
-                      to="/cart"
-                      className={`block px-4 py-2 text-sm ${active ? 'bg-black' : 'text-gray-700'}`}
-                    >
-                      Carrinho de Compra
-                    </Link>
-                  )}
-                </MenuItem>
-                <MenuItem>
-                  {({ active }) => (
-                    <Link
-                      to="#"
-                      onClick={(e) => signout(e)}
-                      className={`block px-4 py-2 text-sm ${active ? 'bg-black' : 'text-gray-700'}`}
-                    >
-                      Sair
-                    </Link>
-                  )}
-                </MenuItem>
+                {!auth?.auth &&
+                  <MenuItem>
+                    {({ active }) => (
+                      <Link
+                        to="/login"
+                        className={`block px-4 py-2 text-sm ${active ? 'bg-black' : 'text-gray-700'}`}
+                      >
+                        Acesse sua Conta
+                      </Link>
+                    )}
+                  </MenuItem>
+                }
+
+                {!auth?.auth &&
+                  <MenuItem>
+                    {({ active }) => (
+                      <Link
+                        to="/cadastro"
+                        className={`block px-4 py-2 text-sm ${active ? 'bg-black' : 'text-gray-700'}`}
+                      >
+                        Criar Conta
+                      </Link>
+                    )}
+                  </MenuItem>
+                }
+
+                {auth?.auth &&
+                  <MenuItem>
+                    {({ active }) => (
+                      <Link
+                        to="/cart"
+                        className={`block px-4 py-2 text-sm ${active ? 'bg-black' : 'text-gray-700'}`}
+                      >
+                        Carrinho de Compra
+                      </Link>
+                    )}
+                  </MenuItem>
+                }
+
+                {auth?.auth &&
+                  <MenuItem>
+                    {({ active }) => (
+                      <Link
+                        to="#"
+                        onClick={(e) => signout(e)}
+                        className={`block px-4 py-2 text-sm ${active ? 'bg-black' : 'text-gray-700'}`}
+                      >
+                        Sair
+                      </Link>
+                    )}
+                  </MenuItem>
+                }
               </div>
             </MenuItems>
           </Menu>
