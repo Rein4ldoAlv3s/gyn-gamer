6d0c2fc344235720655eb6256dad49611e813ee0
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-01-06 21:13:55 -0300

    prod on main

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,15 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import CartProvider from './contexts/CartContext.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
