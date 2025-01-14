1bbe496faf21c7d24855927c2007bb55feb1d704
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-01-14 19:09:59 -0300

    titulo carrinho

--- src/components/CarrinhoCompras.tsx
+++ src/components/CarrinhoCompras.tsx
@@ -75,65 +75,73 @@ const CarrinhoCompras = () => {
     }, [cart]);
 
     return (
-        <div className='flex pb-10'>
-            <TableContainer className='bg-customGrayTable'>
-                <Table>
-                    {/* Cabeçalho da Tabela */}
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align='center'><strong>Produto</strong></TableCell>
-                            <TableCell align='center'><strong>Preço Unidade</strong></TableCell>
-                            <TableCell align='center'><strong>Preço Total</strong></TableCell>
-                            <TableCell align='center'><strong>Quantidade</strong></TableCell>
-                        </TableRow>
-                    </TableHead>
 
-                    {/* Corpo da Tabela */}
-                    <TableBody>
-                        {cart?.produtos.map((product: any) => (
-                            <TableRow key={product.id}>
-                                <TableCell >
-                                    <div className="truncate w-96">
-                                        <Link to={`/produtos/${product.id}`}>
-                                            {product.name}
-                                        </Link>
-                                        {product.name}
-                                        {product.img &&
-                                            <img className='mt-3 h-28' src={product.img} alt="" />
-                                        }
-                                    </div>
-                                </TableCell>
-                                <TableCell >{`R$ ${product.desc.toFixed(2)}`}</TableCell>
-                                <TableCell >{`R$ ${(product.desc * product.quantity).toFixed(2)}`}</TableCell>
-                                <TableCell >
-                                    <IconButton
-                                        size="small"
-                                        onClick={() => handleQuantityChange(product.id, -1)}
-                                        disabled={product.quantity === 0}
-                                    >
-                                        <RemoveIcon />
-                                    </IconButton>
-                                    <span style={{ margin: '0 8px' }}>{product.quantity}</span>
-                                    <IconButton
-                                        size="small"
-                                        onClick={() => handleQuantityChange(product.id, 1)}
-                                        disabled={product.quantity >= product.stock}
-                                    >
-                                        <AddIcon />
-                                    </IconButton>
-                                    <IconButton
-                                        aria-label="delete"
-                                        onClick={() => deleteProduct(product.id)}
-                                    >
-                                        <DeleteIcon />
-                                    </IconButton>
-                                </TableCell>
+        <div>
+            <div>
+                <h1 className="flex justify-center text-3xl font-bold text-customWhite pb-5">
+                    CARRINHO DE COMPRAS
+                </h1>
+            </div>
+            <div className='flex pb-10'>
+                <TableContainer className='bg-customGrayTable'>
+                    <Table>
+                        {/* Cabeçalho da Tabela */}
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align='center'><strong>Produto</strong></TableCell>
+                                <TableCell align='center'><strong>Preço Unidade</strong></TableCell>
+                                <TableCell align='center'><strong>Preço Total</strong></TableCell>
+                                <TableCell align='center'><strong>Quantidade</strong></TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <ResumoCompra qtdTotal={qtdTotal} setQtdTotal={setQtdTotal} valorTotal={valorTotal} setValorTotal={setValorTotal} />
+                        </TableHead>
+
+                        {/* Corpo da Tabela */}
+                        <TableBody>
+                            {cart?.produtos.map((product: any) => (
+                                <TableRow key={product.id}>
+                                    <TableCell >
+                                        <div className="truncate w-96">
+                                            <Link to={`/produtos/${product.id}`}>
+                                                {product.name}
+                                            </Link>
+                                            {product.name}
+                                            {product.img &&
+                                                <img className='mt-3 h-28' src={product.img} alt="" />
+                                            }
+                                        </div>
+                                    </TableCell>
+                                    <TableCell >{`R$ ${product.desc.toFixed(2)}`}</TableCell>
+                                    <TableCell >{`R$ ${(product.desc * product.quantity).toFixed(2)}`}</TableCell>
+                                    <TableCell >
+                                        <IconButton
+                                            size="small"
+                                            onClick={() => handleQuantityChange(product.id, -1)}
+                                            disabled={product.quantity === 0}
+                                        >
+                                            <RemoveIcon />
+                                        </IconButton>
+                                        <span style={{ margin: '0 8px' }}>{product.quantity}</span>
+                                        <IconButton
+                                            size="small"
+                                            onClick={() => handleQuantityChange(product.id, 1)}
+                                            disabled={product.quantity >= product.stock}
+                                        >
+                                            <AddIcon />
+                                        </IconButton>
+                                        <IconButton
+                                            aria-label="delete"
+                                            onClick={() => deleteProduct(product.id)}
+                                        >
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <ResumoCompra qtdTotal={qtdTotal} setQtdTotal={setQtdTotal} valorTotal={valorTotal} setValorTotal={setValorTotal} />
+            </div>
         </div>
     );
 }
