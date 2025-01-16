01e9aa069864f7d99e7bc153f6f92c30137dc001
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-01-16 20:05:00 -0300

    endereco perfil

--- src/components/MyProfile.tsx
+++ src/components/MyProfile.tsx
@@ -4,27 +4,67 @@ const MyProfile = () => {
     return (
         <div>
             <h1 className="flex justify-center text-3xl font-bold text-customWhite">Welcome, User!</h1>
-            <div>
-                <h2>Endereços</h2>
+            <h2>Endereço</h2>
+
+            <div className='flex'>
 
                 <div>
-                    <h2>Cep</h2>
-                    <span>74000-000</span>
+                    <div>
+                        <h2>Nome do destinatário</h2>
+                        <span>Reinaldo Alves</span>
+                    </div>
+
+                    <div>
+                        <h2>Cep</h2>
+                        <span>74000-000</span>
+                    </div>
+
+                    <div>
+                        <h2>Bairro</h2>
+                        <span>Setor Pedro Ludovico</span>
+                    </div>
+
+                    <div>
+                        <h2>Rua/Avenida</h2>
+                        <span>Rua 36</span>
+                    </div>
+
                 </div>
 
                 <div>
-                    <h2>Bairro</h2>
-                    <span>74000-000</span>
-                </div>
+                    <div>
+                        <h2>Estado</h2>
+                        <span>Goiás</span>
+                    </div>
 
-                <div>
-                    <h2>Estado</h2>
-                    <span>Goiás</span>
-                </div>
+                    <div>
+                        <h2>Cidade</h2>
+                        <span>Goiânia</span>
+                    </div>
 
-                <div>
-                    <h2>Cidade</h2>
-                    <span>Goiânia</span>
+                    <div>
+                        <h2>Telefone</h2>
+                        <span>(62) 99755-4481</span>
+                    </div>
+
+                    <div>
+                        <h2>Informações adicionais deste endereço (Opcional)</h2>
+                        <span>Ao lado do Bar do Leão</span>
+                    </div>
+
+                    <div className='mt-3'>
+                        <p>Este é o seu trabalho ou sua casa?</p>
+
+                        <label>
+                            <input type="radio" name="trabalho" value="trabalho" />
+                            Trabalho
+                        </label> <br />
+
+                        <label>
+                            <input type="radio" name="residencia" value="residencia" />
+                            Residencia
+                        </label> <br />
+                    </div>
                 </div>
 
             </div>
