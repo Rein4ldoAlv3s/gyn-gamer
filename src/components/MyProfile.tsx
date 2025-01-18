025d9dc793616563bfb2f9e14d9f1689b3479ee0
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-01-17 21:06:22 -0300

    profile edit

--- src/components/MyProfile.tsx
+++ src/components/MyProfile.tsx
@@ -3,6 +3,8 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { IconButton } from '@mui/material';
 import { HomeIcon } from '@heroicons/react/16/solid';
 import { BiHome } from 'react-icons/bi';
+import { FaHome } from 'react-icons/fa';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 
 const MyProfile = () => {
     return (
@@ -10,15 +12,15 @@ const MyProfile = () => {
             <h1 className="flex justify-center text-3xl font-bold text-customWhite">Welcome, User!</h1>
             <h2 className='text-2xl font-bold'>Endereço de Entrega</h2>
 
-            <div className='mt-2 flex w-1/2 justify-between py-3 px-2 bg-customGrayHover rounded-md text-sm'>
+            <div className='mt-2 flex w-1/2 justify-between bg-customGrayHover rounded-md text-sm'>
 
-                <div>
-                    <div>
-                        <BiHome></BiHome>
-                        <span>Reinaldo Alves</span>
+                <div className='py-3 pl-2'>
+                    <div className='flex items-center'>
+                        <FaHome size={20} className='mr-2 text-white ' />
+                        <span>Reinaldo Alves - (62) 99755-4481</span>
                     </div>
 
-                    <div className='mt-1'>
+                    <div className='mt-2'>
                         <span>Setor Pedro Ludovico, Rua 35</span>
                     </div>
 
@@ -28,15 +30,11 @@ const MyProfile = () => {
 
 
 
-                    <div className='mt-1'>
-                        <span>74000-000</span>
+                    <div className='mt-2'>
+                        <span>CEP 74000-000</span>
                     </div>
 
-                    <div>
-                        <span>(62) 99755-4481</span>
-                    </div>
-
-                    <div className='mt-1'>
+                    <div className='mt-2'>
                         <span>Ao lado do Bar do Leão</span>
                     </div>
 
@@ -47,14 +45,34 @@ const MyProfile = () => {
                 </div>
                 <div>
 
-                    <IconButton
-                    >
-                        <MoreVertIcon />
-                    </IconButton>
+                    <Menu as="div">
+                        <MenuButton className="inline-flex justify-center items-center">
+                            <IconButton>
+                                <MoreVertIcon />
+                            </IconButton>
+                        </MenuButton>
+
+
+                        {/* Menu Items */}
+                        <MenuItems transition modal={false} anchor="bottom start" className="absolute text-white right-0 mt-2 w-56 bg-white rounded-md shadow-lg ring-1  ring-black ring-opacity-5 z-10 transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0">
+                            <div className="py-1">
+                                <MenuItem>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={`block px-4 py-2 text-sm ${active ? 'bg-black' : 'text-gray-700'}`}
+                                        >
+                                            Editar
+                                        </a>
+                                    )}
+                                </MenuItem>
+                            </div>
+                        </MenuItems>
+                    </Menu>
                 </div>
 
             </div>
-        </div>
+        </div >
     )
 }
 
