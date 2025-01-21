bc644c301a50f8382c9de31f0763855b3ceb53aa
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-01-21 14:38:59 -0300

    onchange input

--- src/components/CadastroUsuario.tsx
+++ src/components/CadastroUsuario.tsx
@@ -5,37 +5,35 @@ import Select from "react-select";
 import { Produto, PRODUTOS } from '../constants'
 import e from 'cors';
 
-// export interface Usuario {
-//     nome: string;
-//     usuario: string;
-//     telefone: string;
-//     genero: string;
-//     dto: string;
-//     email: string;
-//     password: string;
-// }
-
 const CadastroUsuario = () => {
 
-    const [nome, setNome] = useState('');
-    const [usuario, setUsuario] = useState('');
-    const [telefone, setTelefone] = useState('');
-    const [genero, setGenero] = useState('');
-    const [dto, setDto] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+
+    const [usuario, setUsuario] = useState({
+        nomeReal: "",
+        nomeUsuario: "",
+        telefone: "",
+        genero: "",
+        dto: "",
+        email: "",
+        password: "",
+    })
 
 
-    const saveUser = (event: any) => {
-        event.prevent.default();
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
 
+        // Atualiza apenas o campo correspondente
+        setUsuario((prevState) => ({
+            ...prevState, // Mantém os outros campos
+            [name]: value, // Atualiza o campo atual
+        }));
     };
 
 
     useEffect(() => {
-        // console.log(genero);
+        console.log(usuario);
 
-    }, [genero]);
+    }, [usuario]);
 
     return (
         <div className=" flex flex-col max-w-lg mx-auto pb-5">
@@ -43,26 +41,28 @@ const CadastroUsuario = () => {
 
             <div className='flex justify-between mt-3'>
                 <div className='w-full mr-2'>
-                    <label htmlFor="nome" className="block text-sm/6 font-medium text-white">
+                    <label htmlFor="nomeReal" className="block text-sm/6 font-medium text-white">
                         Nome Completo
                     </label>
                     <input
-                        id="nome"
-                        name="nome"
+                        id="nomeReal"
+                        name="nomeReal"
                         autoComplete="given-name"
-                        onChange={(e) => setNome(e.target.value)}
+                        onChange={handleChange}
+                        value={usuario.nomeReal}
                         className="mt-2 block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                     />
                 </div>
                 <div className='w-full'>
-                    <label htmlFor="user" className="block text-sm/6 font-medium text-white">
+                    <label htmlFor="nomeUsuario" className="block text-sm/6 font-medium text-white">
                         Nome de Usuário
                     </label>
                     <input
-                        id="user"
-                        name="user"
+                        id="nomeUsuario"
+                        name="nomeUsuario"
                         autoComplete="given-name"
-                        onChange={(e) => setUsuario(e.target.value)}
+                        onChange={handleChange}
+                        value={usuario.nomeUsuario}
                         className="mt-2 block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                     />
                 </div>
@@ -78,7 +78,8 @@ const CadastroUsuario = () => {
                     name="telefone"
                     type="telefone"
                     autoComplete="given-name"
-                    onChange={(e) => setTelefone(e.target.value)}
+                    onChange={handleChange}
+                    value={usuario.telefone}
                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
             </div>
@@ -88,29 +89,29 @@ const CadastroUsuario = () => {
                 <label className='text-sm font-medium text-white' htmlFor='gender'>Selecione o gênero:</label><br />
 
                 <input
-                    onChange={(e) => setGenero(e.target.value)}
                     type="radio"
                     id="masculino"
-                    name="gender"
+                    name="genero"
+                    onChange={handleChange}
                     value="masculino"
                 />
                 <label className='text-sm font-medium text-white' htmlFor="masculino">Masculino</label><br />
 
                 <input
-                    onChange={(e) => setGenero(e.target.value)}
+                    onChange={handleChange}
+                    value="feminino"
                     type="radio"
                     id="feminino"
                     name="gender"
-                    value="feminino"
                 />
                 <label className='text-sm font-medium text-white' htmlFor="feminino">Feminino</label><br />
 
                 <input
-                    onChange={(e) => setGenero(e.target.value)}
+                    onChange={handleChange}
+                    value="outro"
                     type="radio"
                     id="outro"
                     name="gender"
-                    value="outro"
                 />
                 <label className='text-sm font-medium text-white' htmlFor="outro">Outro</label><br />
             </div>
@@ -120,7 +121,8 @@ const CadastroUsuario = () => {
             </label>
             <div className="mt-2">
                 <input
-                    onChange={(e) => setDto(e.target.value)}
+                    onChange={handleChange}
+                    value={usuario.dto}
                     id="dto"
                     name="dto"
                     type="date"
@@ -133,7 +135,8 @@ const CadastroUsuario = () => {
             </label>
             <div className="mt-2">
                 <input
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={handleChange}
+                    value={usuario.email}
                     id="email"
                     name="email"
                     type="email"
@@ -146,7 +149,8 @@ const CadastroUsuario = () => {
             </label>
             <div className="mt-2">
                 <input
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={handleChange}
+                    value={usuario.password}
                     id="password"
                     name="password"
                     type="password"
@@ -155,7 +159,7 @@ const CadastroUsuario = () => {
                 />
             </div>
             <div className='mt-3'>
-                <button onClick={(e) => saveUser(e)} className='bg-black rounded-md text-slate-300 px-3 py-2 w-full '>Criar conta</button>
+                <button className='bg-black rounded-md text-slate-300 px-3 py-2 w-full '>Criar conta</button>
             </div>
 
 
