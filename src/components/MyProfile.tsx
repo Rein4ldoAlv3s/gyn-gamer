2e7bea120cc70c1df9e6dc3fbf775670af5c9ae2
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-02-11 13:25:55 -0300

    dados endereco setados

--- src/components/MyProfile.tsx
+++ src/components/MyProfile.tsx
@@ -12,12 +12,14 @@ const MyProfile = () => {
 
     const idUser = localStorage.getItem("idUser");
     const [userData, setUserData] = useState<any>(null);
+    const [enderecoData, setEnderecoData] = useState<any>(null);
 
     useEffect(() => {
         if (idUser) {
             axios.get('http://localhost:3000/users/' + idUser)
                 .then(function (response) {
                     setUserData(response.data)
+                    setEnderecoData(response.data.Enderecos)
                 })
                 .catch(function (error) {
                     console.error(error);
@@ -26,7 +28,7 @@ const MyProfile = () => {
     }, [])
 
     useEffect(() => {
-        console.log(userData);
+        console.log(enderecoData);
     }, [userData])
 
     return (
@@ -96,65 +98,66 @@ const MyProfile = () => {
             </div>
 
             <h2 className='text-2xl font-bold mt-5'>Endereço de Entrega</h2>
-            <div className='mt-2 flex w-1/2 justify-between bg-customGrayHover rounded-md text-sm'>
 
-                <div className='py-3 pl-2'>
-                    <div className='flex items-center'>
-                        <FaHome size={20} className='mr-2 text-white ' />
-                        <span>Reinaldo Alves</span>
-                    </div>
-
-                    <div className='mt-2'>
-                        <span>Setor Pedro Ludovico, Rua 35</span>
-                    </div>
-
-                    <div>
-                        <span>Goiânia-Goiás</span>
-                    </div>
-
-
-
-                    <div className='mt-2'>
-                        <span>CEP 74000-000</span>
-                    </div>
-
-                    <div className='mt-2'>
-                        <span>Ao lado do Bar do Leão</span>
-                    </div>
-
-                    <div>
-                        <span>Endereço residencial </span>
-                    </div>
-
-                </div>
-                <div>
-
-                    <Menu as="div">
-                        <MenuButton className="inline-flex justify-center items-center">
-                            <IconButton>
-                                <MoreVertIcon />
-                            </IconButton>
-                        </MenuButton>
-
-
-                        {/* Menu Items */}
-                        <MenuItems transition modal={false} anchor="bottom start" className="absolute text-white right-0 mt-2 w-56 bg-white rounded-md shadow-lg ring-1  ring-black ring-opacity-5 z-10 transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0">
-                            <div className="py-1">
-                                <MenuItem>
-                                    {({ active }) => (
-                                        <a
-                                            href="#"
-                                            className={`block px-4 py-2 text-sm ${active ? 'bg-black' : 'text-gray-700'}`}
-                                        >
-                                            Editar
-                                        </a>
-                                    )}
-                                </MenuItem>
+            <div>
+                {enderecoData && enderecoData.map((endereco: any, index: any) => (
+                    <div key={index} className='mt-2 flex w-1/2 justify-between bg-customGrayHover rounded-md text-sm'>
+                        <div className='py-3 pl-2'>
+                            <div className='flex items-center'>
+                                <FaHome size={20} className='mr-2 text-white ' />
+                                <span>{endereco?.nomeDestinatario || ""}</span>
                             </div>
-                        </MenuItems>
-                    </Menu>
-                </div>
 
+                            <div className='mt-2'>
+                                <span>{endereco?.logradouro || ""}, Rua {endereco?.rua || ""}</span>
+                            </div>
+
+                            <div>
+                                <span>{endereco?.cidade || ""}-{endereco?.estado || ""}</span>
+                            </div>
+
+                            <div className='mt-2'>
+                                <span>CEP {endereco?.cep || ""}</span>
+                            </div>
+
+                            <div className='mt-2'>
+                                <span>{endereco?.complemento || ""}</span>
+                            </div>
+
+                            <div>
+                                <span>Endereço {endereco?.tipoEndereco || ""} </span>
+                            </div>
+
+                        </div>
+                        <div>
+
+                            <Menu as="div">
+                                <MenuButton className="inline-flex justify-center items-center">
+                                    <IconButton>
+                                        <MoreVertIcon />
+                                    </IconButton>
+                                </MenuButton>
+
+
+                                {/* Menu Items */}
+                                <MenuItems transition modal={false} anchor="bottom start" className="absolute text-white right-0 mt-2 w-56 bg-white rounded-md shadow-lg ring-1  ring-black ring-opacity-5 z-10 transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0">
+                                    <div className="py-1">
+                                        <MenuItem>
+                                            {({ active }) => (
+                                                <a
+                                                    href="#"
+                                                    className={`block px-4 py-2 text-sm ${active ? 'bg-black' : 'text-gray-700'}`}
+                                                >
+                                                    Editar
+                                                </a>
+                                            )}
+                                        </MenuItem>
+                                    </div>
+                                </MenuItems>
+                            </Menu>
+                        </div>
+                    </div>
+                ))}
             </div>
 
 
