5d2a3beb19d4d1716ddc08ed7d48ec9c24fcdedc
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-02-24 18:22:29 -0300

    handleclickopen

--- src/components/MyProfile.tsx
+++ src/components/MyProfile.tsx
@@ -20,6 +20,16 @@ const MyProfile = () => {
     const [enderecoData, setEnderecoData] = useState<any>(null);
     const [isOpenDialog, setIsOpenDialog] = useState<boolean>(false)
 
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     useEffect(() => {
         // if (idUser) {
         //     axios.get('http://localhost:3000/users/' + idUser)
@@ -183,17 +193,40 @@ const MyProfile = () => {
                                             {({ focus }) => (
                                                 <div>
                                                     <button
+                                                        onClick={handleClickOpen}
                                                         className={`block px-4 py-2 text-sm ${focus ? 'bg-black' : 'text-gray-700'}`}
                                                     >
                                                         Open alert dialog
                                                     </button>
-                                                    <DeleteDialog isOpenDialog></DeleteDialog>
+
                                                 </div>
                                             )}
                                         </MenuItem>
                                     </div>
                                 </MenuItems>
                             </Menu>
+                            <Dialog
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="alert-dialog-title"
+                                aria-describedby="alert-dialog-description"
+                            >
+                                <DialogTitle id="alert-dialog-title">
+                                    {"Use Google's location service?"}
+                                </DialogTitle>
+                                <DialogContent>
+                                    <DialogContentText id="alert-dialog-description">
+                                        Let Google help apps determine location. This means sending anonymous
+                                        location data to Google, even when no apps are running.
+                                    </DialogContentText>
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={handleClose}>Disagree</Button>
+                                    <Button onClick={handleClose} autoFocus>
+                                        Agree
+                                    </Button>
+                                </DialogActions>
+                            </Dialog>
                         </div>
                     </div>
                 ))}
@@ -202,67 +235,6 @@ const MyProfile = () => {
 
         </div >
     )
-}
-
-const DeleteDialog = (isOpenDialog: any) => {
-    const [open, setOpen] = React.useState(false);
-
-    // const handleClickOpen = () => {
-    //     if (focus) {
-    //         setOpen(true);
-    //     }
-
-    // };
-
-    const handleClose = () => {
-        if (!isOpenDialog) {
-            setOpen(false);
-        }
-    };
-
-    useEffect(() => {
-        if (isOpenDialog) {
-            setOpen(true)
-        } else {
-            setOpen(false)
-        }
-
-
-    }, [focus])
-
-
-    return (
-        <React.Fragment>
-            {/* <button
-                onClick={handleClickOpen}
-                className={`block px-4 py-2 text-sm ${focus ? 'bg-black' : 'text-gray-700'}`}
-            >
-                Open alert dialog
-            </button> */}
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {"Use Google's location service?"}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Let Google help apps determine location. This means sending anonymous
-                        location data to Google, even when no apps are running.
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Disagree</Button>
-                    <Button onClick={handleClose} autoFocus>
-                        Agree
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </React.Fragment>
-    );
 }
 
 export default MyProfile
