3b2e504c4aadc52a79e50fbb6dff8c12f2e59f62
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-03-03 19:08:12 -0300

    checkbox clicavel

--- src/components/EscolhaEnderecoEntrega.tsx
+++ src/components/EscolhaEnderecoEntrega.tsx
@@ -17,6 +17,7 @@ const EscolhaEnderecoEntrega = () => {
     const id = 1;
     const idUser = localStorage.getItem("idUser");
     const [enderecoData, setEnderecoData] = useState<any>(null);
+    const [checkboxValue, setCheckboxValue] = useState<any>("dddddd");
 
     const [open, setOpen] = React.useState(false);
 
@@ -63,8 +64,8 @@ const EscolhaEnderecoEntrega = () => {
                 });
         }
 
-        console.log("dsadasdas");
-        console.log(enderecoData);
+        console.log("checkboxValue: ");
+        console.log(checkboxValue);
     }, [])
 
 
@@ -78,23 +79,27 @@ const EscolhaEnderecoEntrega = () => {
                 {enderecoData && enderecoData.map((endereco: any, index: any) => (
                     <div key={index} className='mt-2 flex w-1/2 justify-between bg-customGrayHover rounded-md text-sm'>
                         <div className='py-3 px-2 flex'>
+                            {/* checkbox para selecionar endereço de entrega*/}
                             <div className="inline-flex items-center">
                                 <label className="flex items-center cursor-pointer relative" htmlFor="check-2">
-                                    <input type="checkbox"
+                                    <input type="radio"
                                         className="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-slate-800 checked:border-slate-800"
-                                        id="check-2" />
+                                        onChange={e => console.log(e.target.value)}
+                                        name='opcao'
+                                        value={index}
+                                    />
                                     <span className="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
-                                            stroke="currentColor" stroke-width="1">
-                                            <path fill-rule="evenodd"
+                                            stroke="currentColor" strokeWidth="1">
+                                            <path fillRule="evenodd"
                                                 d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                                clip-rule="evenodd"></path>
+                                                clipRule="evenodd"></path>
                                         </svg>
                                     </span>
                                 </label>
                             </div>
 
-
+                            {/* dados endereço de entrega */}
                             <div className='px-2'>
                                 <div className='flex items-center'>
                                     <FaHome size={20} className='mr-2 text-white ' />
