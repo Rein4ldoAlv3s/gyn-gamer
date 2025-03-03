e1dd03e14b00cd766a0114862d844e5a510c4afe
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-03-02 21:15:26 -0300

    add dialog

--- src/components/EscolhaEnderecoEntrega.tsx
+++ src/components/EscolhaEnderecoEntrega.tsx
@@ -1,15 +1,31 @@
+import React, { useEffect, useState } from 'react'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { IconButton } from '@mui/material';
 import { FaHome } from 'react-icons/fa';
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
-import { useEffect, useState } from 'react';
+import { Button, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import { Link } from 'react-router-dom';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 
 const EscolhaEnderecoEntrega = () => {
 
     const id = 1;
     const idUser = localStorage.getItem("idUser");
     const [enderecoData, setEnderecoData] = useState<any>(null);
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
 
     useEffect(() => {
@@ -41,66 +57,107 @@ const EscolhaEnderecoEntrega = () => {
             <h1 className="flex justify-start text-3xl font-bold text-customWhite ">
                 Escolha a forma de entrega
             </h1>
-            <div className='mt-2 flex w-1/2 justify-between bg-customGrayHover rounded-md text-sm'>
 
-                <div className='py-3 pl-2'>
-                    <div className='flex items-center'>
-                        <FaHome size={20} className='mr-2 text-white ' />
-                        <span>Reinaldo Alves</span>
-                    </div>
-
-                    <div className='mt-2'>
-                        <span>Setor Pedro Ludovico, Rua 35</span>
-                    </div>
-
-                    <div>
-                        <span>Goiânia-Goiás</span>
-                    </div>
-
-
-
-                    <div className='mt-2'>
-                        <span>CEP 74000-000</span>
-                    </div>
-
-                    <div className='mt-2'>
-                        <span>Ao lado do Bar do Leão</span>
-                    </div>
-
-                    <div>
-                        <span>Endereço residencial </span>
-                    </div>
-
-                </div>
-                <div>
-
-                    <Menu as="div">
-                        <MenuButton className="inline-flex justify-center items-center">
-                            <IconButton>
-                                <MoreVertIcon />
-                            </IconButton>
-                        </MenuButton>
-
-
-                        {/* Menu Items */}
-                        <MenuItems transition modal={false} anchor="bottom start" className="absolute text-white right-0 mt-2 w-56 bg-white rounded-md shadow-lg ring-1  ring-black ring-opacity-5 z-10 transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0">
-                            <div className="py-1">
-                                <MenuItem>
-                                    {({ active }) => (
-                                        <Link
-                                            to={`/cadastro-endereco/${id}`}
-                                            className={`block px-4 py-2 text-sm ${active ? 'bg-black' : 'text-gray-700'}`}
-                                        >
-                                            Editar
-                                        </Link>
-                                    )}
-                                </MenuItem>
+            <div>
+                {enderecoData && enderecoData.map((endereco: any, index: any) => (
+                    <div key={index} className='mt-2 flex w-1/2 justify-between bg-customGrayHover rounded-md text-sm'>
+                        <div className='py-3 pl-2'>
+                            <div className='flex items-center'>
+                                <FaHome size={20} className='mr-2 text-white ' />
+                                <span>{endereco?.nomeDestinatario || ""}</span>
                             </div>
-                        </MenuItems>
-                    </Menu>
-                </div>
 
+                            <div className='mt-2'>
+                                <span>{endereco?.logradouro || ""}, Rua {endereco?.rua || ""}</span>
+                            </div>
+
+                            <div>
+                                <span>{endereco?.cidade || ""}-{endereco?.estado || ""}</span>
+                            </div>
+
+                            <div className='mt-2'>
+                                <span>CEP {endereco?.cep || ""}</span>
+                            </div>
+
+                            <div className='mt-2'>
+                                <span>{endereco?.complemento || ""}</span>
+                            </div>
+
+                            <div>
+                                <span>Endereço {endereco?.tipoEndereco || ""} </span>
+                            </div>
+
+                        </div>
+                        <div>
+
+                            <Menu as="div">
+                                <MenuButton className="inline-flex justify-center items-center">
+                                    <IconButton>
+                                        <MoreVertIcon />
+                                    </IconButton>
+                                </MenuButton>
+
+
+                                {/* Menu Items */}
+                                <MenuItems transition modal={false} anchor="bottom start" className="absolute text-white right-0 mt-2 w-56 bg-white rounded-md shadow-lg ring-1  ring-black ring-opacity-5 z-10 transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0">
+                                    <div className="py-1">
+                                        <MenuItem>
+                                            {({ focus }) => (
+                                                <Link
+                                                    to={`/cadastro-endereco/${endereco?.id}`}
+                                                    className={`block px-4 py-2 text-sm ${focus ? 'bg-black' : 'text-gray-700'}`}
+                                                >
+                                                    Editar
+                                                </Link>
+                                            )}
+                                        </MenuItem>
+                                        {/* opens dialog */}
+                                        <MenuItem>
+                                            {({ focus }) => (
+                                                <div>
+                                                    <Link
+                                                        to={""}
+                                                        onClick={handleClickOpen}
+                                                        className={`block px-4 py-2 text-sm ${focus ? 'bg-black' : 'text-gray-700'}`}
+                                                    >
+                                                        Excluir
+                                                    </Link>
+
+                                                </div>
+                                            )}
+                                        </MenuItem>
+                                    </div>
+                                </MenuItems>
+                            </Menu>
+                            {/* abre dialog para confirmar a exclusão do endereço */}
+                            <React.Fragment>
+                                <Dialog
+                                    open={open}
+                                    onClose={handleClose}
+                                    aria-labelledby="alert-dialog-title"
+                                    aria-describedby="alert-dialog-description"
+                                >
+                                    <DialogTitle id="alert-dialog-title">
+                                        {""}
+                                    </DialogTitle>
+                                    <DialogContent>
+                                        <DialogContentText id="alert-dialog-description">
+                                            Tem certeza que deseja excluir esse endereço?
+                                        </DialogContentText>
+                                    </DialogContent>
+                                    <DialogActions>
+                                        <Button >Não</Button>
+                                        <Button autoFocus>
+                                            Sim
+                                        </Button>
+                                    </DialogActions>
+                                </Dialog>
+                            </React.Fragment>
+                        </div>
+                    </div>
+                ))}
             </div>
+
         </div >
     )
 }
