0ec907059d76d5e91586e164cd60fcd2427d6718
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-03-07 19:51:38 -0300

    card boleto

--- src/components/EscolhaPagamento.tsx
+++ src/components/EscolhaPagamento.tsx
@@ -34,18 +34,17 @@ const EscolhaPagamento = () => {
                     <img src="/bf_v6_pix.svg" className='w-[50px]' />
 
                     {/* texto sobre o pix */}
-                    <div className=" ">
-                        <div className='mb-1'>
+                    <div>
+                        <div className='mb-1 font-bold'>
                             <span>Pix</span>
                         </div>
-                        <div className=''>
+                        <div className='font-light'>
                             <span>Aprovação imediata</span>
                         </div>
                     </div>
                 </div>
 
 
-                {/* div pai de um card */}
                 <div className='flex items-center py-3 px-2 mt-2 bg-customGrayHover rounded-md text-sm h-20'>
                     {/* checkbox para selecionar endereço de entrega*/}
                     <div className="inline-flex items-center">
@@ -63,15 +62,17 @@ const EscolhaPagamento = () => {
                     </div>
 
                     {/* imagem do pix */}
-                    <img src="/bf_v6_pix.svg" className='w-[50px]' />
+                    <img src="/bf_v6_boleto.svg" className='w-[50px]' />
 
                     {/* texto sobre o pix */}
-                    <div className=" ">
+                    <div>
                         <div className='mb-1'>
-                            <span>Pix</span>
-                        </div>
-                        <div className=''>
-                            <span>Aprovação imediata</span>
+                            <div className='font-bold'>
+                                <span>Boleto</span>
+                            </div>
+                            <div className='font-light'>
+                                <span>Aprovação em 1 ou 2 dias úteis</span>
+                            </div>
                         </div>
                     </div>
                 </div>
