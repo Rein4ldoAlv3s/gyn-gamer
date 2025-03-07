018b8703f4b38c8f822dede89db3301b1ebd40b3
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-03-07 19:44:34 -0300

    card pix

--- src/components/EscolhaPagamento.tsx
+++ src/components/EscolhaPagamento.tsx
@@ -12,57 +12,74 @@ const EscolhaPagamento = () => {
             </h1>
 
             <div className='w-1/2'>
-                <div className='mt-2 flex justify-between bg-customGrayHover rounded-md text-sm'>
-                    <div className='py-3 px-2 flex'>
-                        {/* checkbox para selecionar endereço de entrega*/}
-                        <div className="inline-flex items-center">
 
-                            <label className="relative flex items-center cursor-pointer">
-                                <input
-                                    name="color"
-                                    type="radio"
-                                    className="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-300 checked:border-slate-400 transition-all"
-                                    readOnly
-                                    onChange={e => setCheckboxValue(e.target.value)}
-                                />
-                                <h2 className='ml-2'>Pix</h2>
-                                <span className="absolute bg-white w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
-                            </label>
+                {/* div pai de um card */}
+                <div className='flex items-center py-3 px-2 mt-2 bg-customGrayHover rounded-md text-sm h-20'>
+                    {/* checkbox para selecionar endereço de entrega*/}
+                    <div className="inline-flex items-center">
+                        <label className="relative flex items-center cursor-pointer">
+                            <input
+                                name="color"
+                                type="radio"
+                                className="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-300 checked:border-slate-400 transition-all"
+                                readOnly
+                                onChange={e => setCheckboxValue(e.target.value)}
+                            />
+                            <span className="absolute bg-white w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
 
-                        </div>
-
-                        <div className="px-2">
-                            <div className='mt-2'>
-                                <span>Av dos Abacates, Rua 11</span>
-                            </div>
-
-                            <div className='mt-2'>
-                                <span>Goiânia-Goiás</span>
-                            </div>
-                        </div>
-
+                        </label>
                     </div>
-                </div>
-                <div className='mt-2 flex justify-between bg-customGrayHover rounded-md text-sm'>
-                    <div className='py-3 px-2 flex'>
-                        {/* checkbox para selecionar endereço de entrega*/}
-                        <div className="inline-flex items-center">
 
-                            <label className="relative flex items-center cursor-pointer">
-                                <input
-                                    name="color"
-                                    type="radio"
-                                    className="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-300 checked:border-slate-400 transition-all"
-                                    readOnly
-                                    onChange={e => setCheckboxValue(e.target.value)}
-                                />
-                                <h2 className='ml-2'>Boleto</h2>
-                                <span className="absolute bg-white w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
-                            </label>
+                    {/* imagem do pix */}
+                    <img src="/bf_v6_pix.svg" className='w-[50px]' />
 
+                    {/* texto sobre o pix */}
+                    <div className=" ">
+                        <div className='mb-1'>
+                            <span>Pix</span>
+                        </div>
+                        <div className=''>
+                            <span>Aprovação imediata</span>
                         </div>
                     </div>
                 </div>
+
+
+                {/* div pai de um card */}
+                <div className='flex items-center py-3 px-2 mt-2 bg-customGrayHover rounded-md text-sm h-20'>
+                    {/* checkbox para selecionar endereço de entrega*/}
+                    <div className="inline-flex items-center">
+                        <label className="relative flex items-center cursor-pointer">
+                            <input
+                                name="color"
+                                type="radio"
+                                className="peer h-5 w-5 cursor-pointer appearance-none rounded-full border border-slate-300 checked:border-slate-400 transition-all"
+                                readOnly
+                                onChange={e => setCheckboxValue(e.target.value)}
+                            />
+                            <span className="absolute bg-white w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
+
+                        </label>
+                    </div>
+
+                    {/* imagem do pix */}
+                    <img src="/bf_v6_pix.svg" className='w-[50px]' />
+
+                    {/* texto sobre o pix */}
+                    <div className=" ">
+                        <div className='mb-1'>
+                            <span>Pix</span>
+                        </div>
+                        <div className=''>
+                            <span>Aprovação imediata</span>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
             </div>
 
         </div>
