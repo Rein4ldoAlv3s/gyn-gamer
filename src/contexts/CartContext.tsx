ec3074021fd0bb30d0686e9755c7a6003f265b4e
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-03-18 17:41:31 -0300

    criar context para pedido

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -8,16 +8,8 @@ interface CartContextType {
     // qtd de um produto (ex: 2 mouses === 2 qtdProduto)
     qtdProduto: number;
     setQtdProduto: React.Dispatch<React.SetStateAction<number>>;
-    // qtd de varios produtos (ex: 2 mouses + 1 teclado + 1 notebook === 4 qtdTotal)
-    // qtdTotal = qtdProduto + qtdProduto + ...
-    // qtdTotal: number[];
-    // setQtdTotal: React.Dispatch<React.SetStateAction<number[]>>;
-    //valor unitario de um produto (4 mouses de R$ 20,00 === R$ 80,00 valorProduto)
-    // valorProduto: number;
-    // setValorProduto: React.Dispatch<React.SetStateAction<number>>;
-    //valor total de varios produtos (1 mouse de R$ 20,00 + 3 fones de ouvido de R$ 50,00 === R$ 170,00 valorTotal)
-    // valorTotal: number[];
-    // setValorTotal: React.Dispatch<React.SetStateAction<number[]>>;
+
+    // criar context oara pedido
 }
 
 // Crie o Contexto com um valor padrão inicial (opcionalmente undefined)
