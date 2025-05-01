e296c0fdd06e9f7db82466d5feec8c0ac64026e0
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-05-01 12:56:38 -0300

    padding card

--- src/components/MeusPedidos.tsx
+++ src/components/MeusPedidos.tsx
@@ -30,65 +30,57 @@ const MeusPedidos = () => {
                     <span className='text-sm'>Você não possui pedidos </span>
                 )}
             </div>
-            <div>
+            <div className='pb-10'>
                 {pedidos?.map((pedido: any, index: number) => (
+                    <div className='w-full mt-2 pb-2 bg-customGrayHover rounded-md text-sm'>
 
-                    <div key={index} className="w-1/2">
-                        {/*detalhe item  */}
-                        <div className='mt-2 bg-customGrayHover rounded-md text-sm'>
-                            <div className=''>
-                                <div className='w-full py-3 px-5 text-sm'>
-                                    <span>{pedido.dataPedido}</span>
-                                </div>
-                            </div>
-                            {pedido.carrinhoCompras.produtos.map((produto: any, index: number) => (
+                        <div className='w-full py-3 px-5'>
+                            <span>{pedido.dataPedido}</span>
+                        </div>
 
-
-                                <div>
-                                    <hr className=" border-[1/2px] border-gray-400" />
-                                    <div className='flex justify-between py-3 px-5'>
-                                        <div className='flex'>
-                                            <div className='p-2 border-[3px] border-white rounded-md bg-white'>
-                                                <img
-                                                    className='w-[80px]'
-                                                    src={produto.img}
-                                                    alt=""
-                                                />
-                                            </div>
-                                            <div className='ml-2 flex flex-col justify-center'>
-                                                <div className='flex'>
-                                                    <span className='text-white'>{produto.name}</span>
-                                                </div>
-                                                <div className='flex mt-1'>
-                                                    <span className='text-white'>R$ {produto.price}</span>
-                                                </div>
-                                                <div className='flex mt-1'>
-                                                    <span className='text-white'>x{produto.quantity}</span>
-                                                </div>
-                                                <div className='flex mt-1'>
-                                                    <span className='text-orange-400'>Aguardando Pagamento</span>
-                                                </div>
-                                            </div>
+                        {pedido.carrinhoCompras.produtos.map((produto: any, index: number) => (
+                            <div>
+                                <hr className=" border-[1/2px] border-gray-400" />
+                                <div className='flex justify-between py-3 px-5'>
+                                    <div className='flex'>
+                                        <div className='p-2 border-[3px] border-white rounded-md bg-white'>
+                                            <img
+                                                className='w-[80px]'
+                                                src={produto.img}
+                                                alt=""
+                                            />
                                         </div>
-                                        <div className='flex flex-col justify-center'>
-                                            <div className='mb-5'>
-                                                <Link to="/detalhe-pedido" className='bg-black rounded-full px-4 py-2'>Ver compra</Link>
+                                        <div className='ml-2 flex flex-col justify-center'>
+                                            <div className='flex'>
+                                                <span className='text-white'>{produto.name}</span>
                                             </div>
-                                            <div>
-                                                <Link to="/produtos/1" className='bg-black rounded-full px-4 py-2'>Comprar novamente</Link>
+                                            <div className='flex mt-1'>
+                                                <span className='text-white'>R$ {produto.price}</span>
                                             </div>
+                                            <div className='flex mt-1'>
+                                                <span className='text-white'>x{produto.quantity}</span>
+                                            </div>
+
                                         </div>
                                     </div>
-
+                                    <div className='flex flex-col justify-center'>
+                                        <div className='mb-5'>
+                                            <Link to="/detalhe-pedido" className='bg-black rounded-full px-4 py-2'>Ver compra</Link>
+                                        </div>
+                                        <div>
+                                            <Link to="/produtos/1" className='bg-black rounded-full px-4 py-2'>Comprar novamente</Link>
+                                        </div>
+                                    </div>
                                 </div>
-                            ))}
-                            <span className='text-white border-[1/2px] border-gray-400'>Total </span>
 
+                            </div>
+                        ))}
+                        <div>
+                            <span className='text-orange-400 pl-5'>Aguardando Pagamento</span>
                         </div>
-                        {/* detalhe envio */}
+                        <span className='text-sm pl-5'>Total: {pedido.carrinhoCompras.precoTotalCompras} </span>
 
                     </div>
-
                 ))}
             </div>
         </div>
